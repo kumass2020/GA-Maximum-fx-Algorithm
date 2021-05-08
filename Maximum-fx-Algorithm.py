43cc65c5b0bc88d6c33c251fd2ae61e45c92204c
hoho <kumass2021@gmail.com>
2021-05-09 05:23:39 +0900

    Update Maximum-fx-Algorithm.py

--- Maximum-fx-Algorithm.py
+++ Maximum-fx-Algorithm.py
@@ -1,11 +1,26 @@
 from math import *
-import numpy
+# import numpy as np
+import random
 
 
 def calc(self, x):
     y = x * sin(x)
     return y
 
+
+v = [0 for i in range(10)]
+print(v)
+for i in range(10):
+    # v[i] = bin(np.random.randint(0, 32))
+    v[i] = format(random.randint(0, 32), 'b')
+    # v[i] = bin(random.randint(0, 32))
+    # format()
+    # print(type(v[i]))
+    # tmp = format(v[i], 'b')
+    print('{0:0>5}'.format(v[i]))
+    # v[i] = initNum
+
+print(v)
 
 
 
